b72e351b02a385b6c81293ace9b0a8748b66d2c5
MiloradGlog <miloradst@hotmail.com>
2019-06-02 21:58:36 +0200

    updated TODO list

--- src/solution/Main.java
+++ src/solution/Main.java
@@ -12,10 +12,15 @@ public class Main {
         new Node(configPath);
 
         /**TODO
-         * 1)Kad cvor dobije nazad radnom cvor, desava se reorganizacija sistema
-         * 1.1)U iz handlerthread dodatog cvora prosledi zahtev za 'razmestanje' cvoru koji si dobio od bootstrapa
-         * 1.2)Implementirati algoritam za 'razmestanje"
-         * 2)Napravi da cim se cvor startuje kontaktira bootstrap
+         * 1)Napravi da cim se cvor startuje kontaktira bootstrap
+         * 2)Napravi jednostavan broadcast
+         * 3)Napravi broadcast nakon leave
+         *  3.1) Cvor koji izlazi posalje broadcast
+         *  3.2) U message zahteva spakuje svog sledbenika
+         *  3.3) Kad broadcast poruka dodje do poslednjeg u prstenu (do cvora kome je sledbenik cvor koji izlazi),
+         *       setuje njegovog sledbenika da bude sledbenik cvora koji izlazi i javi cvoru koji izlazi da je svima uspesno
+         *       javljeno da on napusta mrezu, te on onda bezbedno izlazi.
+         * 4)Napravi thread? i algoritam za izracunavanje problema n kraljica i pocni work stealing.
          * ...
          */
 
